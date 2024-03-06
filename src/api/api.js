b4d2fc8167ba56e122319b5124ad52f6d79a3bc1
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-03-06 06:19:58 +0530

    fix api icon

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-//   baseURL: "https://techutsav-auth-backend.onrender.com",
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_KEY,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
